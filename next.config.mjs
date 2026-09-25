8c77a1d7e9f31ce8bcc68c1db43abbd0a4d4e88a
shubhamsaini <shubham.saini@csimplifyit.com>
2026-09-25 15:45:38 +0530

    Config file changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
+  basePath: "/SachinWedsNikita",
+  assetPrefix: "/SachinWedsNikita/",
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
